d89eda9918af52ec1edd3a82ed1efd3bc3e3b92e
Antonio <aromero94@pm.me>
2026-02-25 22:46:21 +0100

    Afegit el constructor amb console.log a component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ioc-angular-quadrista-antonio-fernandez';
+
+  constructor() {
+    console.log('Projecte Quadrista ben engrasat i funcionant!');
+  }
 }
