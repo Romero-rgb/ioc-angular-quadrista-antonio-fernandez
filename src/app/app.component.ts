b61db9e17662292ff5e4f5bed7baaf045ca04a1e
Antonio <aromero94@pm.me>
2026-03-18 12:53:18 +0100

    Afegit els 10 elements del mockData en froma de graella al app.component fent us de la creació del component llista i del ngfor per enllistar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,17 @@
 import { Component } from '@angular/core';
 import { mockData } from './mocks/dades-mocks';
 import { Element } from './models/element.model';
-import { TargetaElementComponent } from './components/targeta-element/targeta-element.component';
+import { LlistaElementsComponent } from './components/llista-elements/llista-elements.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TargetaElementComponent],
+  imports: [LlistaElementsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ioc-angular-quadrista-antonio-fernandez';
 
-  elementProva: Element = {
-    id: 1,
-    name: 'Element de prova',
-    value: 1,
-    category: 'Test',
-  };
+  elements: Element[] = mockData;
 }
--- src/app/components/llista-elements/llista-elements.component.ts
+++ src/app/components/llista-elements/llista-elements.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TargetaElementComponent } from '../targeta-element/targeta-element.component';
+import { Element } from '../../models/element.model';
 
 @Component({
   selector: 'app-llista-elements',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TargetaElementComponent],
   templateUrl: './llista-elements.component.html',
   styleUrl: './llista-elements.component.scss'
 })
 export class LlistaElementsComponent {
+  @Input({required: true}) elements: Element[] = [];
 
+  trackById(index: number, element: Element): number {
+    return element.id;
+  }
 }
