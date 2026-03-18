b61a785a36dfe3841c7fecce9c33b824df1d1957
Antonio <aromero94@pm.me>
2026-03-18 13:32:50 +0100

    Creada lógica de barra cerca. Establit relació de pare i fills. app.component component pare de barra cerca i de llista elements component que a la vega es pare de Targeta element component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { mockData } from './mocks/dades-mocks';
 import { Element } from './models/element.model';
 import { LlistaElementsComponent } from './components/llista-elements/llista-elements.component';
+import { BarraCercaComponent } from './components/barra-cerca/barra-cerca.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LlistaElementsComponent],
+  imports: [LlistaElementsComponent, BarraCercaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/barra-cerca/barra-cerca.component.ts
+++ src/app/components/barra-cerca/barra-cerca.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-barra-cerca',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './barra-cerca.component.html',
-  styleUrl: './barra-cerca.component.scss'
+  styleUrl: './barra-cerca.component.scss',
 })
 export class BarraCercaComponent {
+  @Output() searchChange = new EventEmitter<string>();
 
+  textSearch = '';
+
+  search(): void {
+    if (this.textSearch.length >= 3) {
+      this.searchChange.emit(this.textSearch);
+    }
+  }
+
+  netejar(): void {
+    this.textSearch = '';
+    this.searchChange.emit('');
+  }
 }
