36228cfaf59521e8d2f6f323cefac5c238d292ea
Antonio <aromero94@pm.me>
2026-03-18 09:16:08 +0100

    Afegit el mock de dades

---
+++ src/app/mocks/dades-mocks.ts
@@ -0,0 +1,15 @@
+import {interElement} from '../models/element.model'
+
+export const mockData: interElement[] = [
+    {id: '0', name: 'David', numericValue: 1, description: 'Descripció 1'},
+    {id: '1', name: 'Antonio', numericValue: 2, description: 'Descripció 2'},
+    {id: '2', name: 'Laura', numericValue: 3, description: 'Descripció 3'},
+    {id: '3', name: 'Maria', numericValue: 4, description: 'Descripció 4'},
+    {id: '4', name: 'Juan', numericValue: 5, description: 'Descripció 5'},
+    {id: '5', name: 'Martin', numericValue: 6, description: 'Descripció 6'},
+    {id: '6', name: 'Eli', numericValue: 7, description: 'Descripció 7'},
+    {id: '7', name: 'Lui', numericValue: 8, description: 'Descripció 8'},
+    {id: '8', name: 'Roberto', numericValue: 9, description: 'Descripció 9'},
+    {id: '9', name: 'Carlos', numericValue: 10, description: 'Descripció 10'},
+
+];
