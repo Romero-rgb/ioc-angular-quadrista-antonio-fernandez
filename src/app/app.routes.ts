6908259a34139b602c78112bd7128dcd8bc153da
Antonio <aromero94@pm.me>
2026-05-01 21:06:58 +0200

    afegides les rutes principals de Quadrista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router'
+import { CatalegPageComponent } from './features/cataleg-page/cataleg-page.component';
+import { CercaComponent } from './features/cerca/cerca.component';
+import { DetallComponent } from './features/detall/detall.component';
+import { PreferitsComponent } from './features/preferits/preferits.component';
+import { LoginComponent } from './features/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'cataleg', pathMatch: 'full'},
+    { path: 'cataleg', component: CatalegPageComponent, title: 'Cataleg'},
+    { path: 'cerca', component: CercaComponent, title: 'Cerca'},
+    { path: 'detall', component: DetallComponent, title: 'Detall'},
+    { path: 'preferits', component: PreferitsComponent, title: 'Preferits'},
+    { path: 'login', component: LoginComponent, title: 'Login'},
+    {path: '**', redirectTo: 'cataleg'},
+];
