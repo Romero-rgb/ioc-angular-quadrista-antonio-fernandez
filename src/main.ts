ff697c01707dc9f091b8deca275b13b0db3cde4c
Antonio <aromero94@pm.me>
2026-05-01 21:08:39 +0200

    configurat el provideRouter(routes) a app.config.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+})
   .catch((err) => console.error(err));
