ea0b3d777b93b096898e8cf241554f8869eea29f
Antonio <aromero94@pm.me>
2026-05-01 20:42:35 +0200

    Creats els components necessaris per començar a enroutar

---
+++ src/app/features/detall/detall.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detall',
+  standalone: true,
+  imports: [],
+  templateUrl: './detall.component.html',
+  styleUrl: './detall.component.scss'
+})
+export class DetallComponent {
+
+}
