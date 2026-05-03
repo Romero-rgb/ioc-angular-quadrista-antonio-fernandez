0ec7b8934aeb0cebd125d793a9f4e04af6562119
Antonio <aromero94@pm.me>
2026-05-03 12:17:57 +0200

    Afegit el hover i el click a la selecció de pàgines de la pàgina principal catàleg. Ara es pot canviar de pàgines clickant, sense haver d'escriure a la barra de navegació

--- src/app/features/detall/detall.component.ts
+++ src/app/features/detall/detall.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detall',
   standalone: true,
   imports: [],
   templateUrl: './detall.component.html',
-  styleUrl: './detall.component.scss'
+  styleUrl: './detall.component.scss',
 })
 export class DetallComponent {
+  elementID: string | null = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.elementID = this.route.snapshot.paramMap.get('id');
+  }
 }
