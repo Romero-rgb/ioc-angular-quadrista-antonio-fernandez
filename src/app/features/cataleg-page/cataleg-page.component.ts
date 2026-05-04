af18fc22220a575618f32648ba2df26da8671d9d
Antonio <aromero94@pm.me>
2026-05-04 17:58:31 +0200

    Afegit el ScrollingModule de la biblioteca AngularCdk per optimitzar la carrega

--- src/app/features/cataleg-page/cataleg-page.component.ts
+++ src/app/features/cataleg-page/cataleg-page.component.ts
@@ -3,11 +3,12 @@ import { Item } from '../../core/models/item.model';
 import { ItemService } from '../../core/services/item.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-cataleg-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollingModule],
   templateUrl: './cataleg-page.component.html',
   styleUrl: './cataleg-page.component.scss',
 })
